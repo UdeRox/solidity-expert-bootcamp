0561bb3bfbb4557f7295e2c7ad0bdcf133526129
udeshika <perera.n.u@gmail.com>
2023-10-12 07:30:10 +0800

    Updated test and script files

--- homework2/script/HomeWork2Script.s.sol
+++ homework2/script/HomeWork2Script.s.sol
@@ -5,11 +5,10 @@ import {Script} from "forge-std/Script.sol";
 import {HomeWork2} from "../src/HomeWork2.sol";
 
 contract HomeWork2Script is Script {
-    uint256[] public initialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    function run() external returns (HomeWork2) {
+    function run(uint256[] calldata _initialArray) external returns (HomeWork2) {
         vm.startBroadcast();
-        HomeWork2 homework2 = new HomeWork2(initialArray);
+        HomeWork2 homework2 = new HomeWork2(_initialArray);
         vm.stopBroadcast();
         return homework2;
     }
--- homework2/test/HomeWork2Test.t.sol
+++ homework2/test/HomeWork2Test.t.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
+import {HomeWork2Script} from "../script/HomeWork2Script.s.sol";
+import {HomeWork2} from "../src/HomeWork2.sol";
 
 contract HomeWork2Test is Test {
-    
+    uint256[] public initialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    HomeWork2 homeWork2;
+
+    function setUp() external {
+        HomeWork2Script homeWork2Script = new HomeWork2Script();
+        homeWork2 = homeWork2Script.run(initialArray);
+    }
+
+    function testDeployer() public {}
 }
