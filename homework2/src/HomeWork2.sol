6d70a45bcebe41d732d95a27b47b31432177f84b
udeshika <perera.n.u@gmail.com>
2023-10-12 08:20:15 +0800

    Updated the remove index logic

--- homework2/src/HomeWork2.sol
+++ homework2/src/HomeWork2.sol
@@ -21,6 +21,8 @@ contract HomeWork2 {
     error ItemNotFoundError();
     error IndexOutBoundError();
 
+    event IndexItemRemoved(uint256 indexed _index, uint indexed _item);
+
     constructor(uint256[] memory _iniDynamicdArray) {
         dynamicArray = _iniDynamicdArray;
     }
@@ -29,13 +31,34 @@ contract HomeWork2 {
     // items are choose randomly
     // after deleting no gaps
     // opt gas
-    function deleteItem(uint256 _deleteItem) public {}
+    function deleteItem(uint256 _deleteItem) public {
+        uint256 indexFound = findIndexOfItem(_deleteItem);
+        if (indexFound != 0) removeIndex(indexFound);
+    }
+
+    //Find the item in the array
+    function findIndexOfItem(uint256 _item) public view returns (uint256) {
+        uint256 arrLength = getArrayLength();
+        for (uint i = 0; i < arrLength; i++) {
+            if (dynamicArray[i] == _item) return i;
+        }
+        revert ItemNotFoundError();
+    }
+
+    function removeIndex(uint256 _removeIndex) public {
+        uint256 lastIndex = getArrayLength() - 1;
+        if (_removeIndex > lastIndex) revert IndexOutBoundError();
+        uint256 item = dynamicArray[_removeIndex];
+        dynamicArray[_removeIndex] = dynamicArray[lastIndex];
+        dynamicArray.pop();
+        emit IndexItemRemoved(_removeIndex, item);
+    }
 
     function setDynamicArray(uint[] memory _dynamicArray) external {
         dynamicArray = _dynamicArray;
     }
 
-    function arrayLength() public view returns (uint256) {
+    function getArrayLength() public view returns (uint256) {
         return dynamicArray.length;
     }
 }
--- homework2/test/HomeWork2Test.t.sol
+++ homework2/test/HomeWork2Test.t.sol
@@ -5,13 +5,17 @@ import {HomeWork2Script} from "../script/HomeWork2Script.s.sol";
 import {HomeWork2} from "../src/HomeWork2.sol";
 
 contract HomeWork2Test is Test {
-    uint256[] public initialArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    uint256[] public INTIAL_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     HomeWork2 homeWork2;
 
     function setUp() external {
         HomeWork2Script homeWork2Script = new HomeWork2Script();
-        homeWork2 = homeWork2Script.run(initialArray);
+        homeWork2 = homeWork2Script.run(INTIAL_ARRAY);
     }
 
     function testDeployer() public {}
+
+    function testArrayLenght() public {
+        assertEq(INTIAL_ARRAY.length, homeWork2.getArrayLength());
+    }
 }
